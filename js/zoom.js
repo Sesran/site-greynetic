285471a73f0a55c5e0da33966cc0460f35ed422c
Muowee <theo.poire@gmail.com>
2018-03-17 19:33:47 +0100

    Ajout du modal pour zoomer sur les images

---
+++ js/zoom.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+// Get the modal
+var modal = $('#myModal');
+
+ // Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.zoom');
+var modalImg = $("#imgModal");
+ // var captionText = document.getElementById("caption");
+img.on("click", function(){
+    modal.css("display", "block");
+    modalImg.attr("src", this.src);
+}); 
+// onclick = function(){
+//    modal.style.display = "block";
+//    modalImg.src = this.src;
+//    captionText.innerHTML = this.alt;
+// }
+
+// Get the <span> element that closes the modal
+
+
+// When the user clicks on <span> (x), close the modal
+modalImg.on('click',function(){
+    modal.css("display","none");
+});
+
+
+});
